c6e249ea67da7706a856561b9733162f9d34e3cc
DanielKim <sshwas@gmail.com>
2019-01-13 23:35:29 +0900

    H1901 passed

--- Softech/Problem/H1901.cpp
+++ Softech/Problem/H1901.cpp
@@ -1,42 +1,56 @@
-#include <cstdio>
-#define MAX(a, b)	((a)>(b) ? (a) : (b))
+#include<iostream>
+using namespace std;
 #define MAXN		(2000000 + 2)
 
-int N, D, A[MAXN], S[MAXN], M[MAXN], maxcnt;
-long long P;
-
-void go(int s, int e, int free)
-{
-	if (s == 0 || e > N || free > (e - D + 1))
-		return;
-	int sum = S[e] - S[s - 1];
-	int minus = M[free];
-	if ((sum - minus) <= P)
-	{
-		maxcnt = MAX(maxcnt, (e - s + 1));
-	}
-}
+int N, D, maxcnt, head, tail, freeidx;
+long long P, S[MAXN], M[MAXN], Mdesc[MAXN];
 
 int main(void)
 {
-	int t;
+	int t, s, e;
 	scanf("%d", &t);
-	for(int tc = 1; tc<=t; tc++)
+	for (int tc = 1; tc <= t; tc++)
 	{
-		scanf("%d %d %d", &N, &P, &D);
-		A[0] = A[N + 1] = 0;
+		scanf("%d %lld %d", &N, &P, &D);
+		S[0] = M[0] = 0;
 		maxcnt = D;
 		for (int i = 1; i <= N; i++)
 		{
-			scanf("%d", &A[i]);
-			S[i] = S[i - 1] + A[i];
+			scanf("%lld", &S[i]);
+			S[i] += S[i - 1];
 		}
 		for (int i = 1; i <= (N - D + 1); i++)
 		{
 			M[i] = S[i + D - 1] - S[i - 1];
 		}
-		go(1, D + 1, 1);
-		printf("#%d %d\n", tc, maxcnt);
+		s = 1, freeidx = 1;
+		head = 0, tail = 1;
+		Mdesc[head] = M[freeidx];
+		while ((s + maxcnt - 1) <= N)
+		{
+			e = s + maxcnt - 1;
+			if ((S[e] - S[s - 1] - Mdesc[head]) <= P)
+			{
+				maxcnt++;
+			}
+			else
+			{
+				if (Mdesc[head] == M[s])
+					head++;
+				s++;
+			}
+			freeidx++;
+			for (int i = tail - 1; i >= head; i--)
+			{
+				if (Mdesc[i] < M[freeidx])
+					tail--;
+				else
+					break;
+			}
+			Mdesc[tail] = M[freeidx];
+			tail++;
+		}
+		printf("#%d %d\n", tc, maxcnt - 1);
 	}
 	return 0;
 }
